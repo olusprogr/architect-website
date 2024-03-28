d9cc4ecc1ae993d0944402b027816737d2bfbb77
olusprogr <olus.main333@gmail.com>
2024-03-28 14:25:58 +0100

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
-import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { CommandListComponent } from './command-list/command-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'about-us', component: AboutUsComponent },
     { path: 'command-list', component: CommandListComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: '**', component: PageNotFoundComponent}
 ];
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,32 +1,19 @@
 import { Component } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    RouterModule
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
   constructor(
-    private router: Router
-  ) { }
-
-  navigateTo(direction: string = 'home') {
-    switch (direction) {
-      case 'home':
-        this.router.navigate(['home']);
-        break;
-      case 'command-list':
-        this.router.navigate(['command-list']);
-        break;
-      case 'about-us':
-        this.router.navigate(['about-us']);
-        break;
-      default: this.router.navigate(['home']);
-    }
+    private router: Router,
+  ) {
   }
 }
